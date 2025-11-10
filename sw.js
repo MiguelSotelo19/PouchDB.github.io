e9e0acd445039f5052dd2bc70e334df8dca8ecc5
Miguel Sotelo <20223TN113@utez.edu.mx>
2025-11-09 22:08:27 -0600

    Actualizado

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
 const CACHE_NAME = 'app-shell-v1';
 
 const APP_SHELLS_ASSETS = [
+    '/',
     '/index.html',
     '/main.js',
     '/app.js',
     '/style.css',
     '/img/192.png',
-    '/img/512.png'
+    '/img/512.png',
+    '/manifest.json',
+    'https://cdn.jsdelivr.net/npm/pouchdb@9.0.0/dist/pouchdb.min.js'
 ]
 
 self.addEventListener("install", (event) => {
