b70012126b831cbb5698c63444145111631983f6
Your Name <you@example.com>
2025-11-10 14:30:27 -0600

    Intento de solucionar error de peticiones: 10

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const CACHE_NAME = 'app-shell-v1';
 
 const APP_SHELLS_ASSETS = [
-    '/',
-    '/index.html',
-    '/main.js',
-    '/app.js',
-    '/style.css',
-    '/img/192.png',
-    '/img/512.png',
-    '/manifest.json',
+    './',
+    './index.html',
+    './main.js',
+    './app.js',
+    './style.css',
+    './img/192.png',
+    './img/512.png',
+    './manifest.json',
     'https://cdn.jsdelivr.net/npm/pouchdb@9.0.0/dist/pouchdb.min.js'
 ]
 
