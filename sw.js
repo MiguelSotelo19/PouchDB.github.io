ca2f8702c801034770453dad713234032badaa5b
Miguel Sotelo <20223TN113@utez.edu.mx>
2025-11-30 21:45:52 -0600

    Terminado creo

--- sw.js
+++ sw.js
@@ -3,10 +3,7 @@ const CACHE_NAME = 'my-cache-v1';
 const URLS = [
     './',
     './index.html',
-    './styles.css',
     './app.js',
-    './img/192.png',
-    './img/512.png',
     './manifest.json',
     'https://cdn.jsdelivr.net/npm/pouchdb@9.0.0/dist/pouchdb.min.js', 
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js',
@@ -25,11 +22,10 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     if(event.request.mode === 'navigate'){
         event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
+            caches.match(event.request)
+                .then((response) => {
+                    return response || fetch(event.request);
+                })
         );
     }
 });
-
